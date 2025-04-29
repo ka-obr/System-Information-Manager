3b098b6cdb23a33d59eed0372c60d958aeca3751
Karol <karol.obrycki11@gmail.com>
2025-04-29 16:45:26 +0200

    Restored the author information

--- systemInfo.sh
+++ systemInfo.sh
@@ -1,3 +1,12 @@
+# Author           : Karol Obrycki
+# Created On       : 29.04.2025
+# Last Modified By : Karol Obrycki
+# Last Modified On : 29.04.2025 
+# Version          : 0.1
+#
+# Licensed under GPL (see /usr/share/common-licenses/GPL for more details
+# or contact # the Free Software Foundation for a copy)
+
 #!/bin/bash
 
 VERSION="1.0"
